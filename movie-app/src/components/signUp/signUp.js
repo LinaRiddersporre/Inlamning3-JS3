ce127491c52a19bad5a5cd3d9d0cff59851c8fcf
Lina <linariddersporre@gmail.com>
2022-11-07 17:13:05 +0100

    mitt i mellan

--- movie-app/src/components/signUp/signUp.js
+++ movie-app/src/components/signUp/signUp.js
@@ -1,10 +1,46 @@
+import { useState } from "react"
+import firebase from 'firebase/app';
+
+// import database from "../firebase/firebase";
+
+
 const SignUp = () => {
+    // const [userName, setUserName] = useState();
+    // const Push = () => {
+    //     database.ref(url).set({
+    //         userName : userName
+    //     }).catch(console.log('det gick inte'))
+    // }
+    
+    const {url} = 'https://movies-and-inlog-js3-default-rtdb.firebaseio.com/inlogg.json'
+
+    fetch(url)
+    .then(response => response)
+    .then(data => {
+        console.log('nehe', data)
+        console.log(firebase.database())
+    })
+
+    const newName = {
+        userName: '',
+        password: ''
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        newName.userName = e.target.children[0].value
+        newName.password = e.target.children[1].value
+       console.log(newName)
+    }
+    
+ 
     return(
         <div>
-            <form>
-                <input type='text' placeholder='@gmail.com'></input>
+            
+            <form onSubmit={submitForm}>
+                <input type='text' placeholder='@gmail.com' ></input>
                 <input type='password' placeholder='Lösenord'></input>
-                <input type='submit' value='Skapa konto'></input>
+                <input type='submit' value='Skapa konto' ></input>
             </form>
         </div>
     )
